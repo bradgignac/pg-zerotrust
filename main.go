2af3ac22af40d9635fdb109fa9d6ba1990b1ac7d
bradgignac <bgignac@bradgignac.com>
2023-08-26 14:01:52 -0400

    Accept TCP connections and immediately terminate them.

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+const UpstreamPort = 5432
+const ProxyPort = 5433
 
 func main() {
-	fmt.Println("Hello World!")
+	addr := fmt.Sprintf(":%d", ProxyPort)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Listening for connections on %d", ProxyPort)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+		}
+
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	log.Printf("Accepted connection")
+
+	// TODO: read from connection until it ends
+	// TODO: write read data over upstream connection. can test with netcat for now
 }
